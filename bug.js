d264904e6522bfe1a36beb7d8a46991c4070401f
BugHunter-X <askzenai@gmail.com>
2025-02-03 11:58:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+
+// Uncommon bug: Incorrect use of middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+app.use((req, res, next) => {
+  //This middleware will always execute as it's placed after the error handling middleware
+  res.send('This should not be executed');
+});
